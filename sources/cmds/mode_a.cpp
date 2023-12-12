eedb5fb89ac78be7fd53826fb03f916b30167536
Arthur Ascedu <aascedu@z4r7p5.42lyon.fr>
2023-12-12 16:50:41 +0100

    Fix backslash n ajoute parfois et parfois pas

--- sources/cmds/mode_a.cpp
+++ sources/cmds/mode_a.cpp
@@ -3,30 +3,22 @@
 void	mode(Server& s, Client& c, std::string& str)
 {
 	std::string target;
-	size_t start = str.find(' ');
-	size_t end = str.find(' ', start + 1);
-	if (str[end - start - 1] == '\n')
-		target = str.substr(start + 1, end - start - 1);
-	else
-		target = str.substr(start + 1, end - start - 1) + "\n";
+	int start = str.find(' ');
+	int end = str.find(' ', start + 1);
+	if (end == -1)
+		end = str.size() - 1;
+	target = str.substr(start + 1, end - start - 1);
+	std::cout << "-" << target << "-" << std::endl;
 	std::vector< Channel* > chan = s.getChannels();
 	std::vector< Channel* >::iterator it;
 	for (it = chan.begin(); it != chan.end(); ++it)
 	{
-		//for (size_t i = 0; i < (*it)->getName().size(); i++)
-		//{
-		//	std::cout << static_cast<int>(((*it)->getName())[i]) << " ";
-		//}
-		std::cout << "-" << (*it)->getName() << "-" << std::endl;
-		std::cout << "-" << target << "-" << std::endl;
 		if ((*it)->getName() == target) {
-			std::cout << "channel inside s : " << (*it)->getName() << " and target : " << target << std::endl;
 			break ;
 		}
 	}
-	if (it == chan.end())
-	{
-		std::cout << "test" << std::endl;
+	if (it == chan.end()) {
+		std::cerr << "DIDNT FIND A CHANNEL NAMED LIKE THIS" << std::endl;
 		return ;
 	}
 }
