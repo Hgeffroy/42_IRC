161ec6baad2fe036f383a5422046957191f54aa0
hgeffroy <henri.geffroy@gmail.com>
2023-12-18 10:40:50 +0100

    INVITE est dans INVITE la dinguerie

--- sources/cmds/quit.cpp
+++ sources/cmds/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: hgeffroy <hgeffroy@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 09:52:42 by hgeffroy          #+#    #+#             */
-/*   Updated: 2023/12/18 09:32:33 by hgeffroy         ###   ########.fr       */
+/*   Updated: 2023/12/18 10:12:17 by hgeffroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,17 @@
  * Enlever le client du server */
 void	quit(Server& s, Client& c, std::string& str)
 {
+	std::map<std::string, Channel*>				channels = s.getChannels();
+	std::map<std::string, Channel*>::iterator	it;
+
+	for (it = channels.begin(); it != channels.end(); ++it)
+	{
+		std::map<std::string, std::string>				members = it->second->getMembers();
+		std::map<std::string, std::string>::iterator	find = members.find(c.getNick());
+
+		if (find != members.end())
+			it->second->removeUserFromChan(find->first);
+	}
+
+	s.removeClient(c.getFd());
 }
