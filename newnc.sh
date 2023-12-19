55810cc0d126d19fa95e7670bae183e2d6c262fb
Thea Wang <twang@z4r7p3.42lyon.fr>
2023-12-19 16:51:30 +0100

    scriptibidou scriptibida script

--- newnc.sh
+++ newnc.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 {
-    echo PASS lalala
-    echo NICK ceci
-    echo USER jfkls 0 \* :jfkdls
-    echo JOIN \#la
-        while true
-        do
-            echo  PRIVMSG \#la :jfkdlsjfkdlsjfkdlsjfkdlsfjldsjfkdls
-        done
+	echo PASS lalala
+	echo NICK ceci
+	echo USER jfkls 0 \* :jfkdls
 } | nc -C localhost 1234
