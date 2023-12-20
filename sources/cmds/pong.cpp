76e7bc932b7c3aab49b4c6f94947352eadb50f64
hgeffroy <henri.geffroy@gmail.com>
2023-12-20 16:43:43 +0100

    Fix segfaults part who pong

--- sources/cmds/pong.cpp
+++ sources/cmds/pong.cpp
@@ -6,7 +6,7 @@
 /*   By: hgeffroy <hgeffroy@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:00:29 by hgeffroy          #+#    #+#             */
-/*   Updated: 2023/12/18 15:12:43 by hgeffroy         ###   ########.fr       */
+/*   Updated: 2023/12/20 16:40:05 by hgeffroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ void	pong(Server& s, Client& c, std::string str)
 	(void)s;
 	std::string	token;
 	size_t	space = str.find(' ');
+
+	if (space == std::string::npos)
+	{
+		sendToClient(c.getFd(), ERR_NEEDMOREPARAMS(c.getNick(), "PONG"));
+		return ;
+	}
 
 	token = str.substr(space + 1);
 	sendToClient(c.getFd(), PONG_MSG(token));
