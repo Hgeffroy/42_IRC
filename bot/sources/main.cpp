4ea4d70f0383f66a4470dd5d269640d30f1dd7b2
Zwhea <theawang@yahoo.com>
2024-01-08 22:15:14 +0100

    add: bot's signals

--- bot/sources/main.cpp
+++ bot/sources/main.cpp
@@ -3,17 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
+/*   By: wangthea <wangthea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 13:53:44 by twang             #+#    #+#             */
-/*   Updated: 2024/01/05 13:18:00 by twang            ###   ########.fr       */
+/*   Updated: 2024/01/08 22:13:03 by wangthea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bot.hpp"
+static void	handle_signals( int signal );
 
 int	main( int ac, char **av )
 {
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, &handle_signals);
+
 	try
 	{
 		if ( ac != 4 )
@@ -27,4 +32,10 @@ int	main( int ac, char **av )
 	}
 
 	return ( 0 );
+}
+
+static void	handle_signals( int signal )
+{
+	if ( signal == SIGINT )
+		throw std::invalid_argument( "SIGINT signal catch.");
 }
